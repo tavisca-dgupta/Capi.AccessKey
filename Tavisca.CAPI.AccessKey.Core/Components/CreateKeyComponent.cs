307c348d5eef759dabde489906e23ac36d316502
tavisca-dmandal <51920311+tavisca-dmandal@users.noreply.github.com>
2019-11-13 12:38:34 +0530

    to be modified if schema is updated

--- Tavisca.CAPI.AccessKey.Core/Components/CreateKeyComponent.cs
+++ Tavisca.CAPI.AccessKey.Core/Components/CreateKeyComponent.cs
@@ -23,7 +23,8 @@ namespace Tavisca.CAPI.AccessKey.Core.Components
 
         public async Task<AccessKeyModel> Create(AccessKeyModel accessKey)
         {
-            if(!(await _databaseAdapter.IsKeyPresent(accessKey.ProgramId)))
+            //clientId to be modified into programId
+            if (!(await _databaseAdapter.IsKeyPresent(accessKey.ClientId)))
             {
                 accessKey.AccessKey = GenerateAccessKey();
                 accessKey.IsKeyActive = false;
