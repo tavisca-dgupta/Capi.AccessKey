14a76af67a7a4081b93f185dcaee1b746ba6124c
tavisca-dmandal <51920311+tavisca-dmandal@users.noreply.github.com>
2019-11-13 18:39:00 +0530

    Added parameterStoreModelTranslatorTest

---
+++ Tavisca.CAPI.AccessKey.UnitTest/CoreTests/TranslatorTests/ParameterStoreModelTranslatorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tavisca.CAPI.AccessKey.Model.Models;
+using Xunit;
+using Tavisca.CAPI.AccessKey.Core.Translator;
+namespace Tavisca.CAPI.AccessKey.UnitTest.CoreTests.TranslatorTests
+{
+    public class ParameterStoreModelTranslatorTests
+    {
+        [Fact]
+        public void ParameterStoreModelTranslator_Success()
+        {
+            var accessKeyModel = GetAccessKeyModel();
+            var parameterStoreModel = accessKeyModel.ToParameterStoreModel();
+            ValidateTranslation(accessKeyModel,parameterStoreModel);
+        }
+
+        private void ValidateTranslation(AccessKeyModel accessKeyModel, ParameterStoreModel parameterStoreModel)
+        {
+            Assert.Equal(accessKeyModel.AccessKey, parameterStoreModel.Key);
+            Assert.Equal(accessKeyModel.ClientTenantId, parameterStoreModel.Value);
+        }
+
+        private AccessKeyModel GetAccessKeyModel()
+        {
+            return new AccessKeyModel()
+            {
+                AccessKey = new Guid().ToString(),
+                IsKeyActive = true,
+                ClientClassicId = "Client_Classic_Id",
+                ClientId = "Client_Id",
+                ClientName = "Client_Name",
+                ClientTenantId = "Client_Tenant_Id",
+                CreatedOn = "Created_On",
+                LastUpdatedOn = "Last_Updated_On",
+                Program = "Program",
+                ProgramGroup = "Program_Group",
+                ProgramId = "Program_Id",
+                UpdatedBy = "Updated_By"
+            };
+        }
+    }
+}
